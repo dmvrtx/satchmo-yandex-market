538511287c5fe59cbe56a668fae9122e8f5e66c0
Valery Sukhomlinov <goodguy@good-guy.me>
2012-04-21 15:36:01 +0100

    Исправления под satchmo 0.9.2

--- satchmo_yandex_market/views.py
+++ satchmo_yandex_market/views.py
@@ -7,19 +7,23 @@ from django.template import RequestContext
 from product.models import Category, Product
 
 from satchmo_yandex_market.models import YandexMarketSite
+from product.utils import find_best_auto_discount
 
 def offers(request, site=None):
     u"""Представление для списка товаров Яндекса"""
     site = get_object_or_404(YandexMarketSite, site=Site.objects.get_current())
     categories = Category.objects.filter(is_active=True, site=site)
+    products = Product.objects.filter(active=True,
+                site = site,
+                category__in = categories,
+                productvariation__parent__isnull=True)
+    sale = find_best_auto_discount(list(products))
     ctx = RequestContext(request, {
             'date' : datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),
             'categories' : categories,
-            'offerproducts' : Product.objects.filter(active=True,
-                site = site,
-                category__in = categories,
-                productvariation__parent__isnull=True),
+            'offerproducts' : products,
             'site' : site,
+            'sale' : sale,
         })
     return render_to_response('yandex_market/offers.html',
             context_instance=ctx)
